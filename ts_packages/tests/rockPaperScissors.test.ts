4fe305ca7829a168f7c9c656b8d64d3cc5c454e0
Jumping Boy <jpregbaha@gmail.com>
2022-01-21 13:24:11 +0100

    Latest tests for RPS

--- ts_packages/tests/rockPaperScissors.test.ts
+++ ts_packages/tests/rockPaperScissors.test.ts
@@ -2,16 +2,38 @@ import { rockPaperScissors } from "../src/rockPaperScissors";
 
 describe("RockPaperScissors game works", () => {
     test("Same choice is a draw", () => {
-        expect(rockPaperScissors("R", "R")).toMatch("Computer chose R. Draw. Nobody wins");
+        expect(rockPaperScissors("R", "R")).toBe("Computer chose Rock. Draw. Nobody wins");
     });
 
-    test("You win", () => {
-        expect(rockPaperScissors("R", "S")).toBe("Computer chose S. You win!");
-    });
-
-    test("Computer wins", () => {
-        expect(rockPaperScissors("S", "R")).toBe("Computer chose R. Computer wins");
+    test("Input is not an option", () => {
+        expect(rockPaperScissors("Rock", "S")).toBe("Wrong input bud");
     });
 });
 
-/* Obviously there should be more test cases but you get the gist */
+describe("You win", () => {
+    test("You win with rock", () => {
+        expect(rockPaperScissors("R", "S")).toBe("Computer chose Scissors. You win!");
+    });
+
+    test("You win with Paper", () => {
+        expect(rockPaperScissors("P", "R")).toBe("Computer chose Rock. You win!");
+    });
+
+    test("You win with scissors", () => {
+        expect(rockPaperScissors("S", "P")).toBe("Computer chose Paper. You win!");
+    });
+});
+
+describe("Computer wins", () => {
+    test("Computer wins with rock", () => {
+        expect(rockPaperScissors("S", "R")).toBe("Computer chose Rock. Computer wins");
+    });
+
+    test("Computer wins with Paper", () => {
+        expect(rockPaperScissors("R", "P")).toBe("Computer chose Paper. Computer wins");
+    });
+
+    test("Computer wins with scissors", () => {
+        expect(rockPaperScissors("P", "S")).toBe("Computer chose Scissors. Computer wins");
+    });
+});
